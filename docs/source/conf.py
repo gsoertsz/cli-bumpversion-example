ee69a105fc712bf1b4ff4a808e82f850a91afcea
Greg <greg.soertsz@gmail.com>
2023-09-14 11:51:13 +1000

    fix(docs): remove plantuml (#8)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,15 +23,6 @@ project = 'cli-bumpversion-example'
 copyright = '2023, Greg Soertsz'
 author = 'Greg Soertsz'
 
-
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
-else:
-    plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
-
-    plantuml_output_format = 'png'
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
